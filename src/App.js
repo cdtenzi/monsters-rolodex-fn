557ad450580ee98750c64a67c5286c4c72f3816c
daniel <cdtenzi@gmail.com>
2022-04-21 12:16:32 +0200

    copied from monsters-rolodex

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+import './App.css';
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchfield: ''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+    .catch(reject => console.log('I couldnt retrieve the list of users'));
+  }
+
+  handleSearch = e => {
+    this.setState({searchfield: e.target.value});
+  }
+
+  render(){
+    const { monsters, searchfield } =this.state; 
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchfield.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder = 'search monsters'
+          handleChange = {this.handleSearch}  
+        />
+        <CardList monsters = {filteredMonsters}>
+        </CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
